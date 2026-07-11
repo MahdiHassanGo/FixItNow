e5daf4030fe8ccc9d97f4a71778c2d8a9fbf4179
MahdiHassanGo <contact.mahdiasif@gmail.com>
2026-07-11 19:00:16 +0600

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
+import "dotenv/config";
 import app from "./app";
+import config from "./config";
 import { prisma } from "./lib/prisma";
-const PORT = process.env.PORT||5000;
-async function main(){
-    try{
-       await prisma.$connect();
-        app.listen(PORT,()=>{
-            console.log(`server is running on port ${PORT}`);
-        })
-    }catch(error){
-      console.error("Error starting server:",error)
-      process.exit(1);
-    }
-}
+
+const main = async () => {
+  try {
+    await prisma.$connect();
+    app.listen(config.port, () => {
+      console.log(`FixItNow server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+};
 
 main();
